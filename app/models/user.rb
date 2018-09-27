2eafa65259d185fac630fac81f1272e5cee5bbed
Patsy Price <pprice@malwarebytes.com>
2018-09-27 16:09:37 -0700

    finish color picker and add validate to color field

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,14 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
   has_many :articles, dependent: :destroy
-  validates :username, presence: true, uniqueness: { case_sensitive: false }, 
+  validates :username, presence: true, uniqueness: { case_sensitive: false },
             length: { minimum: 3, maximum: 25 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 105 },
             uniqueness: { case_sensitive: false },
             format: { with: VALID_EMAIL_REGEX }
+  VALID_COLOR_REGEX = /\A#/
+  validates :color, length: { minimum: 3, maximum: 7 },
+            format: { with: VALID_COLOR_REGEX }
   has_secure_password
 end
